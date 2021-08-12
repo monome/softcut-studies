a7d85cd863377ce75f4b7801ff56fdbb17a30e88
dan derks <derks.dan@gmail.com>
2021-08-12 14:41:53 -0400

    buffer_read_mono is not 0-indexed anymore!

--- 3-cut.lua
+++ 3-cut.lua
@@ -24,7 +24,7 @@ end
 
 function init()
   softcut.buffer_clear()
-  softcut.buffer_read_mono(file,0,1,-1,0,0) --FIXME: ch is 0-indexed
+  softcut.buffer_read_mono(file,0,1,-1,1,1)
 
   for i=1,4 do
     softcut.enable(i,1)
